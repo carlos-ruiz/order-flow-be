7a53bac90d2df4d36e647619693feff89bf8847a
Carlos Ruiz <car.ruiz90@gmail.com>
2025-02-28 09:53:58 -0600

    Fix GitHubAction test 2

--- src/main/java/com/tulipan/ordersapp/OrdersappApplication.java
+++ src/main/java/com/tulipan/ordersapp/OrdersappApplication.java
@@ -4,21 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class OrdersappApplication {
 
-	public static void main(String[] args) {
-		// Cargar el archivo .env
-		Dotenv dotenv = Dotenv.configure().load();
-
-		// Establecer las variables de entorno
-		System.setProperty("MYSQL_ROOT_PASSWORD", dotenv.get("MYSQL_ROOT_PASSWORD"));
-		System.setProperty("MYSQL_DATABASE", dotenv.get("MYSQL_DATABASE"));
-
-		SpringApplication.run(OrdersappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersappApplication.class, args);
+    }
 
 }
--- src/test/java/com/tulipan/ordersapp/OrdersappApplicationTests.java
+++ src/test/java/com/tulipan/ordersapp/OrdersappApplicationTests.java
@@ -1,26 +1,10 @@
 package com.tulipan.ordersapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrdersappApplicationTests {
-
-    @BeforeAll
-    public static void loadEnv() {
-        // Intentar obtener las variables de entorno
-        String rootPassword = System.getenv("MYSQL_ROOT_PASSWORD");
-        String database = System.getenv("MYSQL_DATABASE");
-
-        // Si las variables de entorno no están definidas, cargar desde .env
-        if (rootPassword == null || database == null) {
-            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-            System.setProperty("MYSQL_ROOT_PASSWORD", dotenv.get("MYSQL_ROOT_PASSWORD"));
-            System.setProperty("MYSQL_DATABASE", dotenv.get("MYSQL_DATABASE"));
-        }
-    }
 
     @Test
     void contextLoads() {
